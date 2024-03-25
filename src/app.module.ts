484c1b1c4e5ce07fc30381515c2f33654b133251
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-25 12:53:30 -0300

    🗃️ feat: add typeorm's data source to postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursesModule } from './courses/courses.module';
+import { dataSourceOpts } from './data.source';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || 'root',
-      database: process.env.DB_NAME || 'school',
-      entities: [],
-      migrations: [],
-      migrationsTableName: process.env.DB_MIGRATIONS_TABLE || 'migrations',
-    }),
-    CoursesModule,
-  ],
+  imports: [TypeOrmModule.forRoot(dataSourceOpts), CoursesModule],
   controllers: [],
   providers: [],
 })
---
+++ src/data.source.ts
@@ -0,0 +1,16 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { Course } from './courses/entities/course.entity';
+
+export const dataSourceOpts: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'school',
+  entities: [Course],
+  migrations: ['dist/migrations/*.js'],
+  migrationsTableName: process.env.DB_MIGRATIONS_TABLE || 'migrations',
+};
+
+export default new DataSource(dataSourceOpts);
