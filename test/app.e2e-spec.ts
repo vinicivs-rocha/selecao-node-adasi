13fbb553a961a789d0d4a54260830ba8eb146ced
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-25 13:41:58 -0300

    ✅ test: add course creation testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { AppModule } from 'src/app.module';
+import * as pactum from 'pactum';
+import { AppModule } from '../src/app.module';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -10,8 +11,38 @@ describe('App e2e', () => {
     }).compile();
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+    await app.init();
+    await app.listen(3333);
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
   afterAll(() => app.close());
 
-  it.todo('test');
+  describe('Courses', () => {
+    describe('Create course', () => {
+      it('should respond with bad request when no name is provided', () => {
+        return pactum
+          .spec()
+          .post('/courses')
+          .expectStatus(400)
+          .expectBody({
+            statusCode: 400,
+            message: ['name must be a string'],
+            error: 'Bad Request',
+          });
+      });
+      it('should create a course', async () => {
+        const newCourse = {
+          name: 'Cálculo I',
+        };
+        return pactum
+          .spec()
+          .post('/courses')
+          .withBody(newCourse)
+          .expectStatus(201)
+          .expectJsonMatch({
+            name: 'Cálculo I',
+          });
+      });
+    });
+  });
 });
