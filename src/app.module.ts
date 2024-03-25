02c281fc217061fb6f37a9c3c114a0f53f3b5d0a
Your Name <you@example.com>
2024-03-25 18:36:23 -0300

    ✨ feat: add students module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursesModule } from './courses/courses.module';
 import { dataSourceOpts } from './data.source';
+import { StudentsModule } from './students/students.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOpts), CoursesModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOpts), CoursesModule, StudentsModule],
   controllers: [],
   providers: [],
 })
