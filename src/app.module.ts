de44e2a994dd07897616768fca31d3618378e20d
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-25 11:16:11 -0300

    ➕ chore: setup typeorm
    Setup basic TypeORM with Postgres configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || 'root',
+      database: process.env.DB_NAME || 'school',
+      entities: [],
+      migrations: [],
+      migrationsTableName: process.env.DB_MIGRATIONS_TABLE || 'migrations',
+    }),
+    CoursesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
