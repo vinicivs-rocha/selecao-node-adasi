70a82e27631a590461be984f285b9b7629732d9b
Your Name <you@example.com>
2024-03-25 22:05:09 -0300

    ✨ feat: add Course repository to students module

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Course } from '../courses/entities/course.entity';
 import { Student } from './entities/student.entity';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Student])],
+  imports: [TypeOrmModule.forFeature([Student, Course])],
   controllers: [StudentsController],
   providers: [StudentsService],
 })
