f6b86431a0dfc5e7f379a42e483eb782af6c963e
Your Name <you@example.com>
2024-03-26 17:34:45 -0300

    ✨ feat: add activity finding output boundary

---
+++ src/activities/application/use-cases/find-activity/find-activity.output.ts
@@ -0,0 +1,21 @@
+export class FindActivityOutput {
+  id: string;
+  date: string;
+  scheduledEnd: string;
+  scheduledStart: string;
+  student: {
+    cpf: string;
+    name: string;
+    registration: string;
+    course: {
+      id: string;
+      name: string;
+    };
+  };
+  tasks: {
+    id: string;
+    name: string;
+  }[];
+  actualStart?: string;
+  actualEnd?: string;
+}
