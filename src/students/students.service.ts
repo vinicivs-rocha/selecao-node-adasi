7a81aa7c5f0e9e4772a08592d237ae135ba08ab8
Your Name <you@example.com>
2024-03-25 22:06:02 -0300

    🐛 fix: add course-student related queries

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Course } from '../courses/entities/course.entity';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 import { Student } from './entities/student.entity';
@@ -9,14 +10,33 @@ import { Student } from './entities/student.entity';
 export class StudentsService {
   constructor(
     @InjectRepository(Student) private studentsRepository: Repository<Student>,
+    @InjectRepository(Course) private coursesRepository: Repository<Course>,
   ) {}
 
-  async create(createStudentDto: CreateStudentDto): Promise<Student> {
-    return await this.studentsRepository.save(createStudentDto);
+  async create({ course_id, ...dto }: CreateStudentDto): Promise<Student> {
+    const course = await this.coursesRepository.findOne({
+      where: {
+        id: course_id,
+      },
+      relations: ['students'],
+    });
+
+    if (course === null)
+      throw new NotFoundException(`Course with id ${course_id} not found`);
+
+    const student = new Student();
+    student.cpf = dto.cpf;
+    student.name = dto.name;
+    student.registration = dto.registration;
+    student.course = course;
+
+    return await this.studentsRepository.save(student);
   }
 
   async findAll(): Promise<Student[]> {
-    return this.studentsRepository.find();
+    return this.studentsRepository.find({
+      relations: ['course'],
+    });
   }
 
   async findOne(cpf: string): Promise<Student> {
@@ -24,6 +44,7 @@ export class StudentsService {
       where: {
         cpf,
       },
+      relations: ['course'],
     });
     if (student === null) {
       throw new NotFoundException(`Student with cpf ${cpf} not found`);
@@ -38,12 +59,29 @@ export class StudentsService {
     const student = await this.studentsRepository.findOneBy({ cpf });
     if (student === null)
       throw new NotFoundException(`Student with cpf ${cpf} does not exists`);
-    await this.studentsRepository.update(cpf, updateStudentDto);
 
-    return {
-      ...student,
-      ...updateStudentDto,
-    };
+    const updatedStudent = new Student();
+    updateStudentDto.cpf = updateStudentDto.cpf;
+    updateStudentDto.name = updateStudentDto.name;
+    updateStudentDto.registration = updateStudentDto.registration;
+    if (updateStudentDto.course_id) {
+      const course = await this.coursesRepository.findOne({
+        where: {
+          id: updateStudentDto.course_id,
+        },
+      });
+      if (course === null)
+        throw new NotFoundException(
+          `Course with id ${updateStudentDto.course_id} not found`,
+        );
+      updatedStudent.course = course;
+    }
+    await this.studentsRepository.update(cpf, updatedStudent);
+
+    return (await this.studentsRepository.findOne({
+      where: { cpf },
+      relations: ['course'],
+    })) as Student;
   }
 
   async remove(cpf: string): Promise<{ cpf: string }> {
