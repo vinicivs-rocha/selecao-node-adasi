c51d058d9588f908e70b612024bbff0d8fe49af7
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-26 11:32:59 -0300

    ✨ feat: setup controller and providers on task module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,22 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CreateTaskService } from './application/use-cases/create-task/create-task.service';
+import { DeleteTaskService } from './application/use-cases/delete-task/delete-task.service';
 import { FindTaskService } from './application/use-cases/find-task/find-task.service';
 import { ListTasksService } from './application/use-cases/list-tasks/list-tasks.service';
 import { UpdateTaskService } from './application/use-cases/update-task/update-task.service';
 import { Task } from './domain/entities/task.entity';
 import { CreateTaksController } from './infra/controllers/create-task/create-task.controller';
+import { DeleteTaskController } from './infra/controllers/delete-task/delete-task.controller';
 import { FindTaskController } from './infra/controllers/find-task/find-task.controller';
 import { ListTasksController } from './infra/controllers/list-tasks/list-tasks.controller';
 import { UpdateTaskController } from './infra/controllers/update-task/update-task.controller';
 import { CreateTaskOutputFactory } from './infra/factories/create-output.factory';
 import { FindTaskOutputFactory } from './infra/factories/find-task-output.factory';
 import { ListTasksOutputFactory } from './infra/factories/list-tasks-output.factory';
+import { UpdateTaskOutputFactory } from './infra/factories/update-task-output.factory';
 import { CreateTaskRepository } from './infra/repositories/create-task.repository';
 import { FindTaskRepository } from './infra/repositories/find-task.repository';
 import { ListTasksRepository } from './infra/repositories/list-tasks.repository';
-import { UpdateTaskOutputFactory } from './infra/factories/update-task-output.factory';
 import { UpdateTaskRepository } from './infra/repositories/update-task.repository';
+import { DeleteTaskOutputFactory } from './infra/factories/delete-task-output.factory';
+import { DeleteTaskRepository } from './infra/repositories/delete-task.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task])],
@@ -25,11 +29,14 @@ import { UpdateTaskRepository } from './infra/repositories/update-task.repositor
     ListTasksController,
     FindTaskController,
     UpdateTaskController,
+    DeleteTaskController,
   ],
   providers: [
     CreateTaskService,
     ListTasksService,
     FindTaskService,
+    UpdateTaskService,
+    DeleteTaskService,
     { provide: 'CreateTaskRepository', useClass: CreateTaskRepository },
     { provide: 'CreateTaskOutputFactory', useClass: CreateTaskOutputFactory },
     { provide: 'ListTasksRepository', useClass: ListTasksRepository },
@@ -38,7 +45,8 @@ import { UpdateTaskRepository } from './infra/repositories/update-task.repositor
     { provide: 'FindTaskOutputFactory', useClass: FindTaskOutputFactory },
     { provide: 'UpdateTaskRepository', useClass: UpdateTaskRepository },
     { provide: 'UpdateTaskOutputFactory', useClass: UpdateTaskOutputFactory },
-    UpdateTaskService,
+    { provide: 'DeleteTaskRepository', useClass: DeleteTaskRepository },
+    { provide: 'DeleteTaskOutputFactory', useClass: DeleteTaskOutputFactory },
   ],
 })
 export class TasksModule {}
