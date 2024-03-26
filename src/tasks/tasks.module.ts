859d7df7450c6790445e7336711ee2d665cead77
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-26 09:47:29 -0300

    ✨ feat: setup controllers and providers on task module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CreateTaskService } from './application/use-cases/create-task/create-task.service';
+import { ListTasksService } from './application/use-cases/list-tasks/list-tasks.service';
 import { Task } from './domain/entities/task.entity';
 import { CreateTaksController } from './infra/controllers/create-task/create-task.controller';
+import { ListTasksController } from './infra/controllers/list-tasks/list-tasks.controller';
 import { CreateTaskOutputFactory } from './infra/factories/create-output.factory';
+import { ListTasksOutputFactory } from './infra/factories/list-tasks-output.factory';
 import { CreateTaskRepository } from './infra/repositories/create-task.repository';
+import { ListTasksRepository } from './infra/repositories/list-tasks.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task])],
-  controllers: [CreateTaksController],
+  controllers: [CreateTaksController, ListTasksController],
   providers: [
     CreateTaskService,
     { provide: 'CreateTaskRepository', useClass: CreateTaskRepository },
     { provide: 'CreateTaskOutputFactory', useClass: CreateTaskOutputFactory },
+    { provide: 'ListTasksRepository', useClass: ListTasksRepository },
+    { provide: 'ListTasksOutputFactory', useClass: ListTasksOutputFactory },
+    ListTasksService,
   ],
 })
 export class TasksModule {}
