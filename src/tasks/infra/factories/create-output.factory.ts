2c9d519a022f510985f5dff5175432e3173de329
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-26 09:16:16 -0300

    ✨ feat: add task creation output factory implementation

---
+++ src/tasks/infra/factories/create-output.factory.ts
@@ -0,0 +1,13 @@
+import { ICreateTaskOutputFactory } from 'src/tasks/application/factories/create-output-factory';
+import { CreateTaskOutput } from 'src/tasks/application/use-cases/create-task/create.output';
+import { Task } from 'src/tasks/domain/entities/task.entity';
+
+export class CreateTaskOutputFactory implements ICreateTaskOutputFactory {
+  create(task: Task): CreateTaskOutput {
+    const { id, name } = task;
+    return {
+      id,
+      name,
+    };
+  }
+}
