aba8482225c9d1dac76228cbc3d81855a6e50493
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-26 09:09:00 -0300

    ✨ feat: add task entity

--- src/data.source.ts
+++ src/data.source.ts
@@ -1,6 +1,7 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { Course } from './courses/entities/course.entity';
 import { Student } from './students/entities/student.entity';
+import { Task } from './tasks/domain/entities/task.entity';
 
 export const dataSourceOpts: DataSourceOptions = {
   type: 'postgres',
@@ -9,7 +10,7 @@ export const dataSourceOpts: DataSourceOptions = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'school',
-  entities: [Course, Student],
+  entities: [Course, Student, Task],
   migrations: ['dist/migrations/*.js'],
   migrationsTableName: process.env.DB_MIGRATIONS_TABLE || 'migrations',
 };
