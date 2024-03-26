0b7e9c994519c2e0c66cd97737448920fcce87c4
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-26 09:44:24 -0300

    ✨ feat: add tasks listing use case

---
+++ src/tasks/application/use-cases/list-tasks/list-tasks.service.ts
@@ -0,0 +1,21 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { IListTasksRepository } from 'src/tasks/domain/repositories/list-tasks';
+import { IListTasksOutputFactory } from '../../factories/list-tasks-output-factory';
+import { ListTasksOutput } from './list-tasks.output';
+
+@Injectable()
+export class ListTasksService {
+  constructor(
+    @Inject('ListTasksRepository')
+    private listTasksRepository: IListTasksRepository,
+    @Inject('ListTasksOutputFactory')
+    private listTasksOutputFactory: IListTasksOutputFactory,
+  ) {}
+
+  async execute(): Promise<ListTasksOutput[]> {
+    const tasks = await this.listTasksRepository.listTasks();
+    return tasks.map((task) =>
+      this.listTasksOutputFactory.createListTasksOutput(task),
+    );
+  }
+}
