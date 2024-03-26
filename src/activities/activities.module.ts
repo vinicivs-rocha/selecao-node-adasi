0273bc19dd71da65a1509f29d3bab8529e6643ec
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-26 14:08:52 -0300

    ✨ feat: setup controller and providers on activities module

--- src/activities/activities.module.ts
+++ src/activities/activities.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateActivityService } from './application/use-cases/create-activity/create-activity.service';
+import { Activity } from './domain/entities/activity.entity';
+import { CreateActivityController } from './infra/controllers/create-activity/create-activity.controller';
+import { CreateActivityOutputFactory } from './infra/factories/create-activity-output.factory';
+import { CreateActivityRepository } from './infra/repositories/create-activity.repository';
+import { Student } from 'src/students/entities/student.entity';
+import { Task } from 'src/tasks/domain/entities/task.entity';
 
 @Module({
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([Activity, Student, Task])],
+  controllers: [CreateActivityController],
+  providers: [
+    CreateActivityService,
+    { provide: 'CreateActivityRepository', useClass: CreateActivityRepository },
+    {
+      provide: 'CreateActivityOutputFactory',
+      useClass: CreateActivityOutputFactory,
+    },
+  ],
 })
 export class ActivitiesModule {}
