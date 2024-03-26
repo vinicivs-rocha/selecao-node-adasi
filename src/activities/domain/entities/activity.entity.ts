47483ddaeec3ffc840a8f522b77e115dc936d7fb
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-26 12:15:51 -0300

    ✨ feat: add activity entity

---
+++ src/activities/domain/entities/activity.entity.ts
@@ -0,0 +1,44 @@
+import { Student } from 'src/students/entities/student.entity';
+import { Task } from 'src/tasks/domain/entities/task.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Activity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'date' })
+  date: string;
+
+  @Column({ type: 'timestamptz', name: 'scheduled_start' })
+  scheduledStart: Date;
+
+  @Column({ type: 'timestamptz', name: 'scheduled_end' })
+  scheduledEnd: Date;
+
+  @Column({ type: 'timestamptz', name: 'actual_start', nullable: true })
+  actualStart?: Date;
+
+  @Column({ type: 'timestamptz', name: 'actual_end', nullable: true })
+  actualEnd?: Date;
+
+  @ManyToOne(() => Student, (student) => student.activities)
+  @JoinColumn({ name: 'student_id' })
+  student: Student;
+
+  @ManyToMany(() => Task)
+  @JoinTable({
+    name: 'activity_tasks',
+    joinColumn: { name: 'activity_id' },
+    inverseJoinColumn: { name: 'task_id' },
+  })
+  tasks: Task[];
+}
