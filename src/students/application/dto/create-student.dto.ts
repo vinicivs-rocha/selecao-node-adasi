b39c71016d9ce32bd73e9ccb55f7a2b8d7bb89ad
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-28 09:39:11 -0300

    📝 docs: add regex pattern to student creation dto api property

--- src/students/application/dto/create-student.dto.ts
+++ src/students/application/dto/create-student.dto.ts
@@ -1,8 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsUUID, Matches } from 'class-validator';
 
 export class CreateStudentDto {
   @IsString()
   @Matches(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}$/)
+  @ApiProperty({ pattern: '^[0-9]{3}\\.[0-9]{3}\\.[0-9]{3}-[0-9]{2}$' })
   cpf: string;
 
   @IsString()
