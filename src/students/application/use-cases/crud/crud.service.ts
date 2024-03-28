68dbb2d4dc1e3f5297f1a478fdab9c29d0fd9120
Vinicius Rocha <viniciusroch65@gmail.com>
2024-03-28 09:39:47 -0300

    🐛 fix: import the right student removal output

--- src/students/application/use-cases/crud/crud.service.ts
+++ src/students/application/use-cases/crud/crud.service.ts
@@ -1,11 +1,11 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { RemoveOutput } from 'src/courses/application/use-cases/crud/remove.output';
 import { IStudentsCrudRepository } from 'src/students/domain/repositories/crud';
 import { CreateStudentDto } from '../../dto/create-student.dto';
+import { UpdateStudentDto } from '../../dto/update-student.dto';
 import { IRemoveOutputFactory } from '../../factories/remove-output';
 import { IUpdateOutputFactory } from '../../factories/update-output';
+import { RemoveOutput } from './remove.output';
 import { UpdateOutput } from './update.output';
-import { UpdateStudentDto } from '../../dto/update-student.dto';
 
 @Injectable()
 export class CrudService {
